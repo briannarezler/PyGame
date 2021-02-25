fecfae0967504921f8fc1670840058f187b8c567
briannarezler <brianna_rezler1@baylor.edu>
2021-02-25 14:46:31 -0600

    moving both left and right

--- ship.py
+++ ship.py
@@ -16,13 +16,16 @@ class Ship:
         # start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # movement flag
+        # movement flags
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on the movement flag"""
+        """Update the ship's position based on the movement flags"""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw the ship at its current location"""
